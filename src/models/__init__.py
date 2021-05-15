fc2b77a08b7aaa0a12fd508b9443673679030b1f
deadaf <askpygod@gmail.com>
2021-05-16 00:21:45 +0530

    tag model

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -91,8 +91,28 @@ class Logging(models.Model):
     type = fields.CharEnumField(LogType)
 
 
-#TODO: make logging properties
+# TODO: make logging properties
 # ************************************************************************************************
+
+
+class Tag(models.Model):
+    class Meta:
+        table = "tags"
+
+    id = fields.BigIntField(pk=True)
+    guild_id = fields.BigIntField()
+    name = fields.CharField()
+    content = fields.TextField()
+    is_embed = fields.BooleanField(default=False)
+    is_nsfw = fields.BooleanField(default=False)
+    owner_id = fields.BigIntField()
+    created_at = fields.DatetimeField(auto_now=True)
+    usage = fields.IntField(default=0)
+
+    @property
+    def owner(self):
+        return self.bot.get_user(self.owner_id)
+
 
 # ************************************************************************************************
 
