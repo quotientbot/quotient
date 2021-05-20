139ef5611748cfc14272ef59cd0a07020481313d
deadaf <askpygod@gmail.com>
2021-05-20 19:29:22 +0530

    replaced discord_webhook with discord.Webhook and AsyncWebhookAdapter

--- src/cogs/events/shards.py
+++ src/cogs/events/shards.py
@@ -1,8 +1,8 @@
 from core import Cog, Quotient
 from colorama import Fore, Style, init
-from discord_webhook import DiscordWebhook
 from utils import emote
 import config
+from discord import Webhook, AsyncWebhookAdapter
 
 init(autoreset=True)
 
@@ -16,22 +16,19 @@ class ShardEvents(Cog, name="Shard Events"):
 
     def __init__(self, bot: Quotient):
         self.bot = bot
-
+        self.webhook = Webhook.from_url(config.SHARD_LOG, adapter=AsyncWebhookAdapter(session=self.bot.session))
 
     @Cog.listener()
     async def on_shard_ready(self, shard_id):
         print(Fore.GREEN + f"Launched shard #{shard_id}.")
-        webhook = DiscordWebhook(url=config.SHARD_LOG, content=f'{emote.check} Launched shard #{shard_id} | Total Shards: {len(self.bot.shards)}.')
-        webhook.execute()
+        await self.webhook.send(f"{emote.check} Launched shard #{shard_id} | Total Shards: {len(self.bot.shards)}.")
 
     @Cog.listener()
     async def on_shard_resumed(self, shard_id):
         print(Fore.GREEN + f"Reconnected shard #{shard_id}.")
-        webhook = DiscordWebhook(url=config.SHARD_LOG, content=f'{emote.check} Reconnected shard #{shard_id}.')
-        webhook.execute()
+        await self.webhook.send(f"{emote.check} Reconnected shard #{shard_id}.")
 
     @Cog.listener()
     async def on_shard_disconnect(self, shard_id):
         print(Fore.RED + f"Shard #{shard_id} died.")
-        webhook = DiscordWebhook(url=config.SHARD_LOG, content=f'{emote.error} Shard #{shard_id} died.')
-        webhook.execute()
+        await self.webhook.send(f"{emote.error} Shard #{shard_id} died.")
