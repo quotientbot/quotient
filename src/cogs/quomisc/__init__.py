1f611f33bb3d9958882cf4f25fb06996461e8648
deadaf <askpygod@gmail.com>
2021-05-21 13:24:38 +0530

    quomisc base , color changes

---
+++ src/cogs/quomisc/__init__.py
@@ -0,0 +1,12 @@
+from core import Cog, Quotient, Context
+from .dev import *
+
+
+class Quomisc(Cog):
+    def __init__(self, bot: Quotient):
+        self.bot = bot
+
+
+def setup(bot):
+    bot.add_cog(Quomisc(bot))
+    bot.add_cog(Dev(bot))
