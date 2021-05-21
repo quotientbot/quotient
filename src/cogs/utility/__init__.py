32afd675fc2856e454604049cbbbaa6a82c343da
deadaf <askpygod@gmail.com>
2021-05-21 20:31:57 +0530

    autorole base

--- src/cogs/utility/__init__.py
+++ src/cogs/utility/__init__.py
@@ -1,9 +1,30 @@
 from core import Cog, Quotient, Context
+from discord.ext import commands
+import discord
 
 
-class Utility(Cog):
+class Utility(Cog, name="utility"):
     def __init__(self, bot: Quotient):
         self.bot = bot
+
+    @commands.group()
+    async def autorole(self, ctx, off: str = None):
+        if not off:
+            return await ctx.send_help(ctx.command)
+
+        ...
+
+    @autorole.command(name="humans")
+    async def autorole_humans(self, ctx: Context, *, role: discord.Role):
+        pass
+
+    @autorole.command(name="bots")
+    async def autorole_bots(self, ctx: Context, *, role: discord.Role):
+        pass
+
+    @autorole.command(name="config")
+    async def autorole_config(self, ctx: Context):
+        pass
 
 
 def setup(bot):
