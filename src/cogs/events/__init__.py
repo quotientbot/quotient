c64d32e1c305ae2d11738ea94c6ee0b7dfbd95d5
deadaf <askpygod@gmail.com>
2021-05-21 23:54:26 +0530

    autorole listener

--- src/cogs/events/__init__.py
+++ src/cogs/events/__init__.py
@@ -1,9 +1,11 @@
 from .shards import ShardEvents
 from .main import MainEvents
 from .tasks import QuoTasks
+from .cmds import CmdEvents
 
 
 def setup(bot):
     bot.add_cog(ShardEvents(bot))
     bot.add_cog(MainEvents(bot))
     bot.add_cog(QuoTasks(bot))
+    bot.add_cog(CmdEvents(bot))
