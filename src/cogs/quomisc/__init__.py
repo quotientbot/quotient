55be3ad205f6e8e7b5e8ffd9e4f568dc2f266080
deadaf <askpygod@gmail.com>
2021-05-22 21:45:50 +0530

    THE SOURCE COMMAND

--- src/cogs/quomisc/__init__.py
+++ src/cogs/quomisc/__init__.py
@@ -1,10 +1,35 @@
 from core import Cog, Quotient, Context
+from discord.ext import commands
 from .dev import *
+import inspect
+import os
 
 
 class Quomisc(Cog, name="quomisc"):
     def __init__(self, bot: Quotient):
         self.bot = bot
+
+    @commands.command()
+    async def source(self, ctx: Context, *, search: str = None):
+        """Refer to the source code of the bot commands."""
+        source_url = "https://github.com/quotientbot/Quotient-Bot"
+
+        if search is None:
+            return await ctx.send(source_url)
+
+        command = ctx.bot.get_command(search)
+
+        if not command:
+            return await ctx.send("Couldn't find that command.")
+
+        src = command.callback.__code__
+        filename = src.co_filename
+        lines, firstlineno = inspect.getsourcelines(src)
+
+        location = os.path.relpath(filename).replace("\\", "/")
+
+        final_url = f"<{source_url}/blob/main/src/{location}#L{firstlineno}-L{firstlineno + len(lines) - 1}>"
+        await ctx.send(final_url)
 
 
 def setup(bot):
