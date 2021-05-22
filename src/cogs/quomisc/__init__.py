d11706816b968a59bb542550c00d6d6ae299640f
deadaf <askpygod@gmail.com>
2021-05-23 03:19:10 +0530

    codestats

--- src/cogs/quomisc/__init__.py
+++ src/cogs/quomisc/__init__.py
@@ -1,7 +1,9 @@
 from core import Cog, Quotient, Context
 from discord.ext import commands
 from utils import ColorConverter
+from collections import Counter
 from typing import Optional
+from glob import glob
 from .dev import *
 import inspect
 import os
@@ -32,6 +34,30 @@ class Quomisc(Cog, name="quomisc"):
 
         final_url = f"<{source_url}/blob/main/src/{location}#L{firstlineno}-L{firstlineno + len(lines) - 1}>"
         await ctx.send(final_url)
+
+    @commands.command(aliases=["cs"])
+    async def codestats(self, ctx: Context):
+        """See the code statictics of the bot."""
+        ctr = Counter()
+
+        for ctr["files"], f in enumerate(glob("./**/*.py", recursive=True)):
+            with open(f, encoding="UTF-8") as fp:
+                for ctr["lines"], line in enumerate(fp, ctr["lines"]):
+                    line = line.lstrip()
+                    ctr["imports"] += line.startswith("import") + line.startswith("from")
+                    ctr["classes"] += line.startswith("class")
+                    ctr["comments"] += "#" in line
+                    ctr["functions"] += line.startswith("def")
+                    ctr["coroutines"] += line.startswith("async def")
+                    ctr["docstrings"] += line.startswith('"""') + line.startswith("'''")
+
+        await ctx.send(
+            embed=ctx.bot.embed(
+                ctx,
+                title="Code Stats",
+                description="\n".join([f"**{k.capitalize()}:** {v}" for k, v in ctr.items()]),
+            )
+        )
 
     @commands.command()
     async def prefix(self, ctx, *, new_prefix: Optional[str]):
