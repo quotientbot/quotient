e98c7fe08bd4a28e81354679f12e9361a860bff7
deadaf <askpygod@gmail.com>
2021-05-30 17:21:55 +0530

    unused

--- src/cogs/utility/functions.py
+++ src/cogs/utility/functions.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
-import asyncpg
 from models import Tag
+
+
 class TagName(commands.clean_content):
     def __init__(self, *, lower=False):
         self.lower = lower
@@ -11,33 +12,42 @@ class TagName(commands.clean_content):
         lower = converted.lower().strip()
 
         if not lower:
-            raise commands.BadArgument('Missing tag name.')
+            raise commands.BadArgument("Missing tag name.")
 
         if len(lower) > 100:
-            raise commands.BadArgument('Tag name is a maximum of 100 characters.')
+            raise commands.BadArgument("Tag name is a maximum of 100 characters.")
 
-        first_word, _, _ = lower.partition(' ')
+        first_word, _, _ = lower.partition(" ")
 
-        root = ctx.bot.get_command('tag')
+        root = ctx.bot.get_command("tag")
         if first_word in root.all_commands:
-            raise commands.BadArgument('This tag name starts with a reserved word.')
+            raise commands.BadArgument("This tag name starts with a reserved word.")
 
         return converted if not self.lower else lower
-    
-async def create_tag(ctx, name, content, is_embed=  False, is_nsfw= False):
+
+
+async def create_tag(ctx, name, content, is_embed=False, is_nsfw=False):
 
     query = "SELECT * FROM tags WHERE name = $1 AND guild_id = $2"
     record = await ctx.bot.db.fetchrow(query, name, ctx.guild.id)
-    
+
     if record is None:
-        
-        await Tag.create(guild_id = ctx.guild.id, name=name , content=(str(content).replace("\'", "\"")), owner_id = ctx.author.id, is_nsfw=is_nsfw, is_embed=is_embed)
+
+        await Tag.create(
+            guild_id=ctx.guild.id,
+            name=name,
+            content=(str(content).replace("'", '"')),
+            owner_id=ctx.author.id,
+            is_nsfw=is_nsfw,
+            is_embed=is_embed,
+        )
         return await ctx.success("ban gya")
-    
+
     await ctx.error("pehle se bana hai")
-    
+
+
 async def increment_usage(ctx, name, usage):
     usage += 1
-    
+
     query = "UPDATE tags SET usage = $1 WHERE guild_id = $2 AND name = $3"
     await ctx.bot.db.execute(query, usage, ctx.guild.id, name)
