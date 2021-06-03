33a84b1e17f33d99fd304bd4ec9da37491c4d326
deadaf <askpygod@gmail.com>
2021-06-03 23:25:44 +0530

    notfound ignore maybe

--- src/cogs/events/errors.py
+++ src/cogs/events/errors.py
@@ -16,11 +16,7 @@ class Errors(Cog):
     @Cog.listener()
     async def on_command_error(self, ctx, err):
 
-        ignored = (
-            commands.CommandNotFound,
-            commands.NoPrivateMessage,
-            discord.Forbidden,
-        )
+        ignored = (commands.CommandNotFound, commands.NoPrivateMessage, discord.Forbidden, discord.NotFound)
 
         if isinstance(err, ignored):
             return
