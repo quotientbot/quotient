7bf972c6ea3c326fe23cb9ee1424731e6aa09feb
deadaf <askpygod@gmail.com>
2021-06-07 23:06:11 +0530

    forgot these

--- src/cogs/esports/__init__.py
+++ src/cogs/esports/__init__.py
@@ -545,6 +545,7 @@ class ScrimManager(Cog, name="Esports"):
 
     @smanager.group(name="reserve", invoke_without_command=True)
     @commands.max_concurrency(1, BucketType.guild)
+    @checks.can_use_sm()
     @commands.bot_has_permissions(embed_links=True, manage_messages=True)
     async def s_reserve(self, ctx, scrim: ScrimConverter):
         """
@@ -554,6 +555,7 @@ class ScrimManager(Cog, name="Esports"):
         await menu.start(ctx)
 
     @s_reserve.command(name="list", aliases=("all",))
+    @checks.can_use_sm()
     async def s_reverse_list(self, ctx, scrim: ScrimConverter):
         """
         Get a list of all reserved teams and their leaders.
@@ -572,6 +574,8 @@ class ScrimManager(Cog, name="Esports"):
         await ctx.send(embed=embed)
 
     @smanager.command(name="autoclean")
+    @checks.can_use_sm()
+    @commands.bot_has_permissions(embed_links=True, manage_messages=True)
     async def s_autoclean(self, ctx, scrim: ScrimConverter):
         """Commands related to quotient's autoclean"""
         await AutocleanMenu(scrim=scrim).start(ctx)
