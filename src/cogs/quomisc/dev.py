8d70c13f3081b77adc4c70103eafcc4299c76986
deadaf <askpygod@gmail.com>
2021-06-08 15:06:23 +0530

    cmd history

--- src/cogs/quomisc/dev.py
+++ src/cogs/quomisc/dev.py
@@ -1,10 +1,11 @@
 from prettytable import PrettyTable
 from core import Cog, Quotient, Context
 from discord.ext import commands
+from .helper import tabulate_query
 from time import perf_counter as pf
 from models.models import Commands
 from utils import get_ipm
-
+import typing, datetime
 
 __all__ = ("Dev",)
 
@@ -60,3 +61,103 @@ class Dev(Cog):
         embed.set_footer(text="Total Commands: {}  | Invoke rate per minute: {}".format(cmds, round(get_ipm(ctx.bot), 2)))
 
         await ctx.send(embed=embed)
+
+    @commands.group(hidden=True, invoke_without_command=True)
+    async def command_history(self, ctx):
+        """Command history."""
+        query = """SELECT
+                        CASE failed
+                            WHEN TRUE THEN cmd || ' [!]'
+                            ELSE cmd
+                        END AS "cmd",
+                        to_char(used_at, 'Mon DD HH12:MI:SS AM') AS "invoked",
+                        user_id,
+                        guild_id
+                   FROM commands
+                   ORDER BY used_at DESC
+                   LIMIT 15;
+                """
+        await tabulate_query(ctx, query)
+
+    @command_history.command(name="for")
+    async def command_history_for(self, ctx, days: typing.Optional[int] = 7, *, command: str):
+        """Command history for a command."""
+
+        query = """SELECT *, t.success + t.failed AS "total"
+                   FROM (
+                       SELECT guild_id,
+                              SUM(CASE WHEN failed THEN 0 ELSE 1 END) AS "success",
+                              SUM(CASE WHEN failed THEN 1 ELSE 0 END) AS "failed"
+                       FROM commands
+                       WHERE cmd=$1
+                       AND used_at > (CURRENT_TIMESTAMP - $2::interval)
+                       GROUP BY guild_id
+                   ) AS t
+                   ORDER BY "total" DESC
+                   LIMIT 30;
+                """
+
+        await tabulate_query(ctx, query, command, datetime.timedelta(days=days))
+
+    @command_history.command(name="guild", aliases=["server"])
+    async def command_history_guild(self, ctx, guild_id: int):
+        """Command history for a guild."""
+
+        query = """SELECT
+                        CASE failed
+                            WHEN TRUE THEN cmd || ' [!]'
+                            ELSE cmd
+                        END AS "cmd",
+                        channel_id,
+                        user_id,
+                        used_at
+                   FROM commands
+                   WHERE guild_id=$1
+                   ORDER BY used_at DESC
+                   LIMIT 15;
+                """
+        await tabulate_query(ctx, query, guild_id)
+
+    @command_history.command(name="user", aliases=["member"])
+    @commands.is_owner()
+    async def command_history_user(self, ctx, user_id: int):
+        """Command history for a user."""
+
+        query = """SELECT
+                        CASE failed
+                            WHEN TRUE THEN cmd || ' [!]'
+                            ELSE cmd
+                        END AS "cmd",
+                        guild_id,
+                        used_at
+                   FROM commands
+                   WHERE user_id=$1
+                   ORDER BY used_at DESC
+                   LIMIT 20;
+                """
+        await tabulate_query(ctx, query, user_id)
+
+    @command_history.command(name="cog")
+    async def command_history_cog(self, ctx, days: typing.Optional[int] = 7, *, cog: str = None):
+        """Command history for a cog or grouped by a cog."""
+
+        interval = datetime.timedelta(days=days)
+        if cog is not None:
+            cog = self.bot.get_cog(cog)
+            if cog is None:
+                return await ctx.send(f"Unknown cog: {cog}")
+
+            query = """SELECT *, t.success + t.failed AS "total"
+                       FROM (
+                           SELECT command,
+                                  SUM(CASE WHEN failed THEN 0 ELSE 1 END) AS "success",
+                                  SUM(CASE WHEN failed THEN 1 ELSE 0 END) AS "failed"
+                           FROM commands
+                           WHERE cmd = any($1::text[])
+                           AND used_at > (CURRENT_TIMESTAMP - $2::interval)
+                           GROUP BY cmd
+                       ) AS t
+                       ORDER BY "total" DESC
+                       LIMIT 30;
+                    """
+            return await tabulate_query(ctx, query, [c.qualified_name for c in cog.walk_commands()], interval)
---
+++ src/cogs/quomisc/helper.py
@@ -0,0 +1,73 @@
+import discord, io
+
+
+class TabularData:
+    def __init__(self):
+        self._widths = []
+        self._columns = []
+        self._rows = []
+
+    def set_columns(self, columns):
+        self._columns = columns
+        self._widths = [len(c) + 2 for c in columns]
+
+    def add_row(self, row):
+        rows = [str(r) for r in row]
+        self._rows.append(rows)
+        for index, element in enumerate(rows):
+            width = len(element) + 2
+            if width > self._widths[index]:
+                self._widths[index] = width
+
+    def add_rows(self, rows):
+        for row in rows:
+            self.add_row(row)
+
+    def render(self):
+        """Renders a table in rST format.
+        Example:
+        +-------+-----+
+        | Name  | Age |
+        +-------+-----+
+        | Alice | 24  |
+        |  Bob  | 19  |
+        +-------+-----+
+        """
+
+        sep = "+".join("-" * w for w in self._widths)
+        sep = f"+{sep}+"
+
+        to_draw = [sep]
+
+        def get_entry(d):
+            elem = "|".join(f"{e:^{self._widths[i]}}" for i, e in enumerate(d))
+            return f"|{elem}|"
+
+        to_draw.append(get_entry(self._columns))
+        to_draw.append(sep)
+
+        for row in self._rows:
+            to_draw.append(get_entry(row))
+
+        to_draw.append(sep)
+        return "\n".join(to_draw)
+
+
+async def tabulate_query(ctx, query, *args):
+    records = await ctx.db.fetch(query, *args)
+
+    if len(records) == 0:
+        return await ctx.send("No results found.")
+
+    headers = list(records[0].keys())
+    table = TabularData()
+    table.set_columns(headers)
+    table.add_rows(list(r.values()) for r in records)
+    render = table.render()
+
+    fmt = f"```\n{render}\n```"
+    if len(fmt) > 2000:
+        fp = io.BytesIO(fmt.encode("utf-8"))
+        await ctx.send("Too many results...", file=discord.File(fp, "results.txt"))
+    else:
+        await ctx.send(fmt)
