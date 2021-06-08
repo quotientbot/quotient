a901efa3969c155a2686f2621faa7f0e1098e2b2
deadaf <askpygod@gmail.com>
2021-06-08 14:16:00 +0530

    typo

--- src/models/models.py
+++ src/models/models.py
@@ -321,7 +321,7 @@ class Commands(models.Model):
     channel_id = fields.BigIntField()
     user_id = fields.BigIntField(index=True)
     cmd = fields.CharField(max_length=100, index=True)
-    used_at = fields.DatetimeField(autonow=True, null=True)
+    used_at = fields.DatetimeField(auto_now=True)
     prefix = fields.CharField(max_length=20)
     failed = fields.BooleanField(default=False)
 
