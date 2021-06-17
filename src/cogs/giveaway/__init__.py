98f5ed6282752b0a995fedba29a4f2b61f6342b2
deadaf <askpygod@gmail.com>
2021-06-17 20:00:52 +0530

    gquick brief

--- src/cogs/giveaway/__init__.py
+++ src/cogs/giveaway/__init__.py
@@ -129,7 +129,7 @@ class Giveaways(Cog):
     @commands.command()
     @commands.has_permissions(manage_messages=True, add_reactions=True, embed_links=True)
     async def gquick(self, ctx: Context, duration: FutureTime, winners: int, *, prize: str):
-
+        """Quickly create a giveaway in current channel."""
         count = await Giveaway.filter(guild_id=ctx.guild.id, ended_at__not_isnull=True).count()
         if count >= 5 and not await ctx.is_premium_guild():
             return await ctx.error(
