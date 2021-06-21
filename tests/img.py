cfd6e381b904118c841e1134f3e96e12327a9a60
deadaf <askpygod@gmail.com>
2021-06-21 13:42:41 +0530

    tests done, removed clutter

--- tests/img.py
+++ tests/img.py
@@ -1,108 +1,71 @@
 from PIL import ImageDraw, Image, ImageFont
 
 
-_dict = {
-    "quotient": (4, 80, 86, 166),
-    "butterfly": (8, 134, 56, 112),
-    "4pandas": (12, 40, 32, 72),
-    "kite": (16, 40, 26, 66),
-}
+def add_watermark(image):
+    text = "Quotient • quotientbot.xyz"
+    draw = ImageDraw.Draw(image)
+
+    font = ImageFont.truetype("myfont.ttf", 25)
+    textwidth, textheight = draw.textsize(text, font)
+
+    margin = 20
+    x = width - textwidth - margin
+    y = height - textheight - margin
+
+    draw.text((x, y), text, font=font)
 
 
-image = Image.open("background.jpg")
+def add_title(image):
+    text = "Bahot-Hard ESPORTS"
+    font = ImageFont.truetype("theboldfont.ttf", 90)
+
+    d1 = ImageDraw.Draw(image)
+    w, h = d1.textsize(text, font)
+
+    left = (image.width - w) / 2
+    top = 50
+
+    d1.text((left, top), text, font=font)
+
+    # second title
+    w, h = d1.textsize("Overall Standings", font)
+    left = (image.width - w) / 2
+    d1.text((left, 150), "Overall Standings", font=font)
+
+
+def add_rectangles(image, rect):
+    top = 320
+    rect = rect.resize((round(rect.size[0] / 2.8), round(rect.size[1] / 2.8)))
+
+    image.paste(rect, (40, 260), rect)
+    for i in range(10):
+        image.paste(rect, (40, top), rect)
+        top += 50
+
+    top = 273
+    d1 = ImageDraw.Draw(image)
+    font = ImageFont.truetype("theboldfont.ttf", 30)
+    d1.text((55, top), "Rank", (0, 0, 0), font=font)
+    d1.text((300, top), "Team Name", (0, 0, 0), font=font)
+    d1.text((652, top), "Posi Pt.", (0, 0, 0), font=font)
+    d1.text((800, top), "Kill Pt.", (0, 0, 0), font=font)
+    d1.text((950, top), "Total", (0, 0, 0), font=font)
+    d1.text((1100, top), "Win", (0, 0, 0), font=font)
+
+
+image = Image.open("test.jpg")
 rect = Image.open("rectangle.png")
+
+
+rect = rect.convert("RGBA")
 image = image.resize((1250, 938))
 
 width, height = image.size
 
-text = "FANATIC ESPORTS"
-font = ImageFont.truetype("theboldfont.ttf", 90)
 
-# adding title
-d1 = ImageDraw.Draw(image)
-w, h = d1.textsize(text, font)
-
-left = (image.width - w) / 2
-top = 50
-
-d1.text((left, top), text, font=font)
+add_title(image)
+add_watermark(image)
+add_rectangles(image, rect)
 
 
-# second title
-w, h = d1.textsize("Overall Standings", font)
-left = (image.width - w) / 2
-d1.text((left, 150), "Overall Standings", font=font)
-
-# adding rectangles
-rect = rect.convert("RGBA")
-
-top = 320
-rect = rect.resize((round(rect.size[0] / 2.8), round(rect.size[1] / 2.8)))
-
-for i in range(10):
-    image.paste(rect, (40, top), rect)
-    top += 50
-
-
-image.paste(rect, (40, 260), rect)
-d1 = ImageDraw.Draw(rect)
-font = ImageFont.truetype("myfont.ttf", 500)
-d1.text((45, 260), "Rank", (0, 0, 0), font=font)
-
-
-# watermark
-text = "Quotient • quotientbot.xyz"
-draw = ImageDraw.Draw(image)
-
-font = ImageFont.truetype("myfont.ttf", 25)
-textwidth, textheight = draw.textsize(text, font)
-
-margin = 20
-x = width - textwidth - margin
-y = height - textheight - margin
-
-draw.text((x, y), text, font=font)
-
-image.save("final.jpg")
-
-###########################################################
-# centering the title
-
-# text = "VIXON LODA"
-# font = ImageFont.truetype("theboldfont.ttf", 90)
-
-# d1 = ImageDraw.Draw(image)
-
-
-# w, h = d1.textsize(text, font)
-
-# left = (image.width - w) / 2
-# top = 100
-
-# d1.text((left, top), text, font=font)
-
-# image.show()
-# image.save("vixon.jpg")
-
-################################################################
-# watermark stuff
-
-# width, height = img.size
-
-# draw = ImageDraw.Draw(img)
-
-# text = "Quotient • quotientbot.xyz"
-
-
-# font = ImageFont.truetype("myfont.ttf", 25)
-# textwidth, textheight = draw.textsize(text, font)
-
-# margin = 20
-# x = width - textwidth - margin
-# y = height - textheight - margin
-
-# draw.text((x, y), text, font=font)
-# img.show()
-
-# img.save("watermark.jpg")
-################################################################
+image.show()
---
+++ tests/img2.py
@@ -0,0 +1,119 @@
+from PIL import Image, ImageDraw, ImageFont
+
+
+def add_watermark(image):
+    text = "Quotient • quotientbot.xyz"
+    draw = ImageDraw.Draw(image)
+
+    font = ImageFont.truetype("myfont.ttf", 25)
+    textwidth, textheight = draw.textsize(text, font)
+
+    margin = 20
+    x = width - textwidth - margin
+    y = height - textheight - margin
+
+    draw.text((x, y), text, font=font)
+
+
+def add_title(image):
+    text = "Yo Bro ESPORTS"
+    font = ImageFont.truetype("theboldfont.ttf", 90)
+
+    d1 = ImageDraw.Draw(image)
+    w, h = d1.textsize(text, font)
+
+    left = (image.width - w) / 2
+    top = 50
+
+    d1.text((left, top), text, font=font)
+
+    # second title
+    w, h = d1.textsize("Overall Standings", font)
+    left = (image.width - w) / 2
+    d1.text((left, 150), "Overall Standings", font=font)
+
+
+image = Image.open("rectangle.png")
+image = image.convert("RGBA")
+
+image = image.resize((round(image.size[0] / 2.8), round(image.size[1] / 2.8)))
+
+draw = ImageDraw.Draw(image)
+font = ImageFont.truetype("roboto/italic.ttf", 30)
+
+top = 8
+fill = (0, 0, 0)
+
+draw.text((18, top), "Rank", fill, font=font)
+draw.text((250, top), "Team Name", fill, font=font)
+draw.text((610, top), "Place Pt", fill, font=font)
+draw.text((770, top), "Kills Pt", fill, font=font)
+
+draw.text((905, top), "Total Pt", fill, font=font)
+
+draw.text((1060, top), "Win?", fill, font=font)
+
+_list = []
+
+_list.append(image)
+
+
+_dict = {
+    "quotient": [1, 20, 20, 40],
+    "butterfly": [2, 14, 14, 28],
+    "4pandas": [3, 10, 8, 18],
+    "kite": [4, 10, 5, 15],
+    "quotient2": [1, 20, 20, 40],
+    "butterfly2": [2, 14, 14, 28],
+    "4pandas2": [3, 10, 8, 18],
+    "kite2": [4, 10, 5, 15],
+    "quotient3": [1, 20, 20, 40],
+    "butterfly3": [2, 14, 14, 28],
+}
+
+font = ImageFont.truetype("roboto/Roboto-Bold.ttf", 30)
+
+top = 10
+left = 35
+
+for idx, item in enumerate(_dict.items(), start=1):
+
+    image = Image.open("rectangle.png")
+    image = image.convert("RGBA")
+
+    image = image.resize((round(image.size[0] / 2.8), round(image.size[1] / 2.8)))
+
+    draw = ImageDraw.Draw(image)
+
+    team = item[0]
+    win, place, kill, total = item[1]
+
+    draw.text((left, top), f"#{idx:02}", fill, font=font)
+    draw.text((left + 150, top), f"Team {team.title()}", fill, font=font)
+    draw.text((left + 610, top), str(place), fill, font=font)
+    draw.text((left + 760, top), str(kill), fill, font=font)
+    draw.text((left + 897, top), str(total), fill, font=font)
+    draw.text((left + 1025, top), f"{'Yes!' if win else 'No!'}", fill, font=font)
+
+    _list.append(image)
+
+
+image = Image.open("test.jpg")
+image = image.resize((1250, 938))
+width, height = image.size
+top = 320
+
+for i in _list:
+    if _list.index(i) == 0:
+        image.paste(i, (40, 260), i)
+    else:
+        image.paste(i, (40, top), i)
+        top += 50
+
+add_watermark(image)
+add_title(image)
+
+
+image.save("points.jpg")
+# for image in _list:
+#     image.show()
