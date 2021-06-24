2c6e2071a960a95714a18cd7edc891b0edb1f9e3
Dagger <69048874+Dagger-py@users.noreply.github.com>
2021-06-24 15:18:45 +0530

    Update example_config.py

--- src/example_config.py
+++ src/example_config.py
@@ -17,9 +17,19 @@ COLOR = 0x00FFB3  # default color
 
 FOOTER = "quo is lub!"  # default footer
 
+PREFIX = "q"
+
+SERVER_LINK = ""
+
+BOT_INVITE = ""
+
+WEBSITE = ""
+
+REPOSITORY = ""
 
 DEVS = ()  # mrko b lagne laga hai tuple better hai
 
 # LOGS
 SHARD_LOG = ""
 ERROR_LOG = ""  # should I make these accessible to all, I am not sure :C
+PUBLIC_LOG =""
