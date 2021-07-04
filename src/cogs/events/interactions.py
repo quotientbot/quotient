3f32bd3b6a0b177ce8df66b29146743e4b30d8c7
deadaf <askpygod@gmail.com>
2021-07-04 14:59:19 +0530

    ipc scrim routes

--- src/cogs/events/interactions.py
+++ src/cogs/events/interactions.py
@@ -2,7 +2,7 @@ import discord
 from core import Cog
 from constants import IST
 from discord.ext import ipc
-from models import Guild, Scrim
+from models import Guild, Scrim, Timer
 from datetime import datetime, timedelta
 
 
@@ -134,6 +134,136 @@ class IpcRoutes(Cog):
     @ipc.server.route()
     async def edit_scrim(self, payload):
         data = payload.data
+
+        guild = self.bot.get_guild(int(data.get("guild_id")))
+        scrim = await Scrim.get_or_none(id=int(data.get("id")))
+
+        if not all((guild, scrim)):
+            return self.deny_request("Either this scrim was deleted or Quotient was removed from the server.")
+
+        _dict = {
+            "name": data.get("name"),
+            "required_mentions": int(data.get("required_mentions")),
+            "start_from": int(data.get("start_from")),
+            "total_slots": int(data.get("total_slots")),
+            "autoslotlist": data.get("autoslotlist"),
+            "multiregister": data.get("multiregister"),
+            "autodelete_rejects": data.get("autodelete_rejects"),
+            "teamname_compulsion": data.get("teamname_compulsion"),
+            "no_duplicate_name": data.get("no_duplicate_name"),
+            "show_time_elapsed": data.get("show_time_elapsed"),
+            "open_message": data.get("open_message"),
+            "close_message": data.get("close_message"),
+        }
+
+        registration_channel_id = int(data.get("registration_channel_id"))
+        if not scrim.registration_channel_id == registration_channel_id:
+            registration_channel = self.bot.get_channel(int(payload.data.get("registration_channel_id")))
+
+            if not registration_channel:
+                return self.deny_creation(
+                    "Quotient cannot see registration channel , Make sure it has appropriate permissions."
+                )
+
+            perms = registration_channel.permissions_for(guild.me)
+            if not all(
+                (
+                    perms.send_messages,
+                    perms.manage_messages,
+                    perms.manage_channels,
+                    perms.add_reactions,
+                    perms.embed_links,
+                )
+            ):
+                return self.deny_creation(
+                    "Quotient do not have required permissions in the registration channel."
+                )
+
+            if await Scrim.filter(registration_channel_id=registration_channel.id):
+                return self.deny_creation(
+                    "The registration channel you selected is already assigned to another scrim."
+                )
+
+            _dict["registration_channel_id"] = registration_channel.id
+
+        slotlist_channel_id = int(data.get("slotlist_channel_id"))
+        if not scrim.slotlist_channel_id == slotlist_channel_id:
+            slotlist_channel = self.bot.get_channel(int(payload.data.get("slotlist_channel_id")))
+            if not slotlist_channel:
+                return await self.deny_creation(
+                    payload, "Quotient cannot see slotlist channel , Can you make sure it has appropriate permissions?"
+                )
+
+            perms = slotlist_channel.permissions_for(guild.me)
+            if not all(
+                (
+                    perms.send_messages,
+                    perms.manage_messages,
+                    perms.manage_channels,
+                    perms.add_reactions,
+                    perms.embed_links,
+                )
+            ):
+                return await self.deny_creation(
+                    payload, "Quotient do not have required permissions in the Slotlist channel."
+                )
+
+            _dict["slotlist_channel_id"] = slotlist_channel_id
+
+        role_id = int(data.get("role_id"))
+        if not scrim.role_id == role_id:
+            role = guild.get_role(int(payload.data.get("role_id")))
+
+            _list = [
+                k
+                for k, v in dict(role.permissions).items()
+                if v is True
+                and k in ("manage_channels", "manage_guild", "manage_messages", "manage_roles", "administrator")
+            ]
+            if _list:
+                return await self.deny_creation(
+                    payload, "Success role contains some moderation permissions, kindly remove them first."
+                )
+
+            _dict["role_id"] = role.id
+
+        ping_role_id = data.get("ping_role_id")
+        if ping_role_id and int(ping_role_id) != scrim.ping_role_id:
+            _dict["ping_role_id"] = int(ping_role_id)
+
+        _dict["open_role_id"] = int(data.get("open_role_id"))
+
+        epoch = datetime(1970, 1, 1)
+
+        open_time = IST.localize(epoch + timedelta(hours=5, minutes=30, milliseconds=int(data.get("open_time"))))
+
+        _dict["open_time"] = open_time
+
+        if data.get("autoclean_time"):
+            autoclean_time = IST.localize(
+                epoch + timedelta(hours=5, minutes=30, milliseconds=int(data.get("autoclean_time")))
+            )
+            if datetime.now(tz=IST) > autoclean_time:
+                autoclean_time = autoclean_time + timedelta(hours=24)
+
+            _dict["autoclean_time"] = autoclean_time
+            await self.bot.reminders.create_timer(autoclean_time, "autoclean", scrim_id=int(data.get("id")))
+
+
+        await Scrim.filter(id=int(data.get("id"))).update(**_dict)
+        await self.bot.db.execute(
+            """UPDATE public."sm.scrims" SET autoclean = $1 , open_days = $2 WHERE id = $3""",
+            data.get("autoclean"),
+            data.get("open_days"),
+            int(data.get("id")),
+        )
+        await Timer.filter(
+            extra={"args": [], "kwargs": {"scrim_id": int(data.get("id"))}}, event__in=("scrim_open", "autoclean")
+        ).delete()
+
+        await self.bot.reminders.create_timer(open_time, "scrim_open", scrim_id=int(data.get("id")))
+
+        return self.positive
 
     @ipc.server.route()
     async def delete_scrim(self, payload):
