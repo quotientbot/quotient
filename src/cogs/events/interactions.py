7207638f9ef3bf166a607dad966dfd7d4402a9e3
deadaf <askpygod@gmail.com>
2021-07-20 15:36:16 +0530

    moved idp route to concerned file

--- src/cogs/events/interactions.py
+++ src/cogs/events/interactions.py
@@ -1,14 +1,9 @@
-from contextlib import suppress
-import asyncio
 import discord
 from core import Cog
 from constants import IST
 from discord.ext import ipc
 from models import Guild, Scrim, Timer
 from datetime import datetime, timedelta
-
-from models.esports import SSVerify
-
 
 class IpcRoutes(Cog):
     def __init__(self, bot):
@@ -269,71 +264,4 @@ class IpcRoutes(Cog):
     @ipc.server.route()
     async def delete_scrim(self, payload):
         await Scrim.filter(id=int(payload.scrim_id)).delete()
-        return self.positive
-
-    @ipc.server.route()
-    async def update_guild_settings(self, payload):
-        guild_id = payload.guild_id
-
-        guild = await Guild.get(guild_id=int(guild_id))
-
-        self.bot.guild_data[guild.guild_id] = {
-            "prefix": guild.prefix,
-            "color": guild.embed_color,
-            "footer": guild.embed_footer,
-        }
-        return self.positive
-
-    async def delete_idp_message(self, message: discord.Message, seconds):
-        with suppress(AttributeError, discord.HTTPException, discord.NotFound, discord.Forbidden):
-            await asyncio.sleep(seconds)
-            await message.delete()
-
-    @ipc.server.route()
-    async def send_idp(self, payload):
-        data = payload.data
-
-        guild = self.bot.get_guild(int(data.get("guild_id")))
-        if not guild:
-            return self.deny_request("Quotient was removed from your server.")
-
-        channel = guild.get_channel(int(data.get("channel_id")))
-        if not channel:
-            return self.deny_request(
-                "Quotient cannot see send `Id/pass channel`, kindly make sure it has appropriate permissions."
-            )
-
-        perms = channel.permissions_for(guild.me)
-        if not all((perms.send_messages, perms.embed_links)):
-            return self.deny_request(
-                f"Kindly make sure Quotient has `send_messages` and `embed_links` permission in {str(channel)}"
-            )
-
-        embed = discord.Embed.from_dict(data.get("embed"))
-
-        ping_role_id = data.get("ping_role_id")
-
-        role = None
-        if ping_role_id:
-            role = getattr(guild.get_role(int(ping_role_id)), "mention", "")
-
-        msg = await channel.send(
-            content=role if role else "",
-            embed=embed,
-            allowed_mentions=discord.AllowedMentions(roles=True),
-        )
-        delete_in = data.get("delete_in")
-        if delete_in:
-            self.bot.loop.create_task(self.delete_idp_message(msg, int(delete_in) * 30))
-
-        if data.get("slotlist"):
-            scrim_id = int(data.get("scrim_id"))
-            if scrim_id:
-                scrim = await Scrim.get_or_none(id=scrim_id, guild_id=guild.id)
-                if scrim and await scrim.teams_registered.count():
-                    embed, schannel = await scrim.create_slotlist()
-                    smsg = await channel.send(embed=embed)
-                    if delete_in:
-                        self.bot.loop.create_task(self.delete_idp_message(smsg, int(delete_in) * 30))
-
         return self.positive
