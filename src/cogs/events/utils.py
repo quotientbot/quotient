6db39668a1edd7148f81472e0eb8899337a47a8d
deadaf <askpygod@gmail.com>
2021-08-30 13:05:48 +0530

    fixed erase guild method

--- src/cogs/events/utils.py
+++ src/cogs/events/utils.py
@@ -1,11 +1,15 @@
 from models import *
 
-async def erase_guild(guild_id:int):
+
+async def erase_guild(guild_id: int):
     check = await Guild.get_or_none(guild_id=guild_id)
     if check and not check.is_premium:
         await Guild.filter(guild_id=guild_id).delete()
 
-    await Scrim.filter(guild_id=guild_id).delete()
+    scrims = await Scrim.filter(guild_id=guild_id)
+
+    await Scrim.filter(pk__in=(scrim.id for scrim in scrims)).delete()
+
     await Tourney.filter(guild_id=guild_id).delete()
     await Autorole.filter(guild_id=guild_id).delete()
     await Tag.filter(guild_id=guild_id).delete()
@@ -15,5 +19,6 @@ async def erase_guild(guild_id:int):
     await Partner.filter(guild_id=guild_id).delete()
     await AutoPurge.filter(guild_id=guild_id).delete()
     await SlotManager.filter(guild_id=guild_id).delete()
+    await SlotLocks.filter(pk__in=(scrim.id for scrim in scrims)).delete()
     await EasyTag.filter(guild_id=guild_id).delete()
     await TagCheck.filter(guild_id=guild_id).delete()
