9994f17965d526e9541034716c61869cfe92be79
deadaf <askpygod@gmail.com>
2021-09-06 16:30:04 +0530

    check member role

---
+++ src/core/route_helper/dashboard.py
@@ -0,0 +1,16 @@
+
+
+async def check_member_role(bot, data):
+    g_id,m_id,role_id = data['guild_id'], data['member_id'], data['role_id']
+
+    guild = bot.get_guild(g_id)
+    if not guild:
+        return {"ok":True, "result":False}
+
+    member = guild.get_member(m_id)
+    if not member or not role_id in (role.id for role in member.roles):
+        return {"ok":True, "result":False}
+    
+    return {"ok":True, "result":True}
+
+    
