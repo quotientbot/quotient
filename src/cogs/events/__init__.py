dbef314d2c6c4f8320ab6ee5db7295da4c247bbc
deadaf <askpygod@gmail.com>
2021-12-04 22:40:48 +0530

    refactoring VotesCog

--- src/cogs/events/__init__.py
+++ src/cogs/events/__init__.py
@@ -2,7 +2,7 @@ from .shards import ShardEvents
 from .main import MainEvents
 from .tasks import QuoTasks
 from .cmds import CmdEvents
-from .votes import Votes
+from .votes import VotesCog
 from .errors import Errors
 
 
@@ -11,5 +11,5 @@ def setup(bot):
     bot.add_cog(MainEvents(bot))
     bot.add_cog(QuoTasks(bot))
     bot.add_cog(CmdEvents(bot))
-    bot.add_cog(Votes(bot))
+    bot.add_cog(VotesCog(bot))
     bot.add_cog(Errors(bot))
--- src/cogs/events/votes.py
+++ src/cogs/events/votes.py
@@ -1,21 +1,24 @@
+from __future__ import annotations
+import typing
+
+if typing.TYPE_CHECKING:
+    from core import Quotient
+
 from discord import Webhook
 from datetime import datetime, timedelta
-from core import Cog, Quotient
+from core import Cog
+
+from models import Votes, User, Guild, Premium, Timer
 from contextlib import suppress
+
 import constants
-import models, discord
-
-from utils.time import strtime
+import discord
 
 
-class Votes(Cog):
+class VotesCog(Cog):
     def __init__(self, bot: Quotient):
         self.bot = bot
         self.hook = Webhook.from_url(self.bot.config.PUBLIC_LOG, session=self.bot.session)
-
-    @property
-    def reminders(self):
-        return self.bot.get_cog("Reminders")
 
     @Cog.listener()
     async def on_member_join(self, member: discord.Member):
@@ -23,40 +26,47 @@ class Votes(Cog):
         if not member.guild or not member.guild.id == self.bot.server.id:
             return
 
-        if await models.Votes.get(user_id=member.id, is_voter=True).exists():
+        if await Votes.get(user_id=member.id, is_voter=True).exists():
             await member.add_roles(discord.Object(id=self.bot.config.VOTER_ROLE))
 
-        if await models.User.get(pk=member.id, is_premium=True).exists():
+        if await User.get(pk=member.id, is_premium=True).exists():
             await member.add_roles(discord.Object(id=self.bot.config.PREMIUM_ROLE))
 
     @Cog.listener()
-    async def on_vote(self, record: models.Votes):
-        await models.Votes.filter(user_id=record.user_id).update(notified=True)
-        await self.reminders.create_timer(record.expire_time, "vote", user_id=record.user_id)
+    async def on_vote(self, record: Votes):
+        await Votes.get(user_id=record.pk).update(notified=True)
+        await self.bot.reminders.create_timer(record.expire_time, "vote", user_id=record.user_id)
+
         member = self.bot.server.get_member(record.user_id)
         if member is not None:
             await member.add_roles(discord.Object(id=self.bot.config.VOTER_ROLE), reason="They voted for me.")
 
-        member = member if member is not None else await self.bot.fetch_user(record.user_id)
+        else:
+            member = await self.bot.getch(self.bot.get_user, self.bot.fetch_user, record.pk)
+
         with suppress(discord.HTTPException, AttributeError):
-            record = await models.Votes.get(pk=record.user_id)
+            await record.refresh_from_db()
+
             embed = discord.Embed(color=discord.Color.green(), description=f"Thanks **{member}** for voting.")
             embed.set_image(url=constants.random_thanks())
             embed.set_footer(text=f"Your total votes: {record.total_votes}")
             await self.hook.send(embed=embed, username="vote-logs", avatar_url=self.bot.user.avatar.url)
 
     @Cog.listener()
-    async def on_vote_timer_complete(self, timer: models.Timer):
+    async def on_vote_timer_complete(self, timer: Timer):
         user_id = timer.kwargs["user_id"]
-        vote = await models.Votes.filter(user_id=user_id).first()
+        vote = await Votes.get(user_id=user_id)
+
+        await Votes.get(pk=user_id).update(is_voter=False, notified=False)
 
         member = self.bot.server.get_member(user_id)
         if member is not None:
             await member.remove_roles(discord.Object(id=self.bot.config.VOTER_ROLE), reason="Their vote expired.")
 
-        member = member if member is not None else await self.bot.fetch_user(user_id)
-        if vote.reminder:
+        else:
+            member = await self.bot.getch(self.bot.get_user, self.bot.fetch_user, user_id)
 
+        if vote.reminder:
             embed = discord.Embed(
                 color=self.bot.color,
                 description=f"{constants.random_greeting()}, You asked me to remind you to vote.",
@@ -68,11 +78,9 @@ class Votes(Cog):
             except:
                 pass
 
-        await models.Votes.filter(user_id=user_id).update(is_voter=False, notified=False)
-
     @Cog.listener()
-    async def on_premium_purchase(self, record: models.Premium):
-        await models.Premium.filter(order_id=record.order_id).update(is_notified=True)
+    async def on_premium_purchase(self, record: Premium):
+        await Premium.filter(order_id=record.order_id).update(is_notified=True)
         member = self.bot.server.get_member(record.user_id)
         if member is not None:
             await member.add_roles(discord.Object(id=self.bot.config.PREMIUM_ROLE), reason="They purchased premium.")
@@ -105,6 +113,6 @@ class Votes(Cog):
         except:
             pass
 
-        await self.reminders.create_timer(
+        await self.bot.reminders.create_timer(
             datetime.now(tz=constants.IST) + timedelta(days=30), "user_premium", user_id=record.user_id
         )
