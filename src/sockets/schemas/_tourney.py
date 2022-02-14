313fbc4b09f71f4ca20a94aa42b1ca8669f0955a
deadaf <askpygod@gmail.com>
2022-02-14 13:18:24 +0530

    sockets tourney model

---
+++ src/sockets/schemas/_tourney.py
@@ -0,0 +1,22 @@
+from typing import List
+from pydantic import BaseModel
+
+
+class SockTourney(BaseModel):
+    id: int = None
+    guild_id: int
+    name: str = "Quotient-Tourney"
+    registration_channel_id: int
+    confirm_channel_id: int
+    role_id: int
+    required_mentions: int = 4
+    total_slots: int
+    banned_users: List[int]
+    host_id: int
+    multiregister: bool = False
+    open_role_id: int = None
+    teamname_compulsion: bool = False
+    ping_role_id: int = None
+    no_duplicate_name: bool = True
+    autodelete_rejected: bool = True
+    success_message: str = None
