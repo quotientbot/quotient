3ca6367b7487ef603326b7fba2891bbdebea0eb4
deadaf <askpygod@gmail.com>
2022-03-13 15:57:27 +0530

    page buttons

--- src/cogs/esports/views/groupm/_paginator.py
+++ src/cogs/esports/views/groupm/_paginator.py
@@ -3,14 +3,40 @@ import typing as T
 
 from ..base import EsportsBaseView
 from core import Context
+import discord
 
 
 class GroupPages(EsportsBaseView):
-    def __init__(self, ctx: Context, *, ping_role=True, **kwargs):
+    def __init__(self, ctx: Context, *, ping_role=True, category=None):
         super().__init__(ctx)
 
         self.ping_role = ping_role
         self.current_page = 1
 
+        self.category: discord.CategoryChannel = category
+
     async def render(self):
+        ...
+
+    async def get_page(self):
+        ...
+
+    @discord.ui.button()
+    async def prev_button(self, button: discord.Button, interaction: discord.Interaction):
+        ...
+
+    @discord.ui.button()
+    async def skip_to(self, button: discord.Button, interaction: discord.Interaction):
+        ...
+
+    @discord.ui.button()
+    async def next_button(self, button: discord.Button, interaction: discord.Interaction):
+        ...
+
+    @discord.ui.button()
+    async def send_channel(self, button: discord.Button, interaction: discord.Interaction):
+        ...
+
+    @discord.ui.button()
+    async def send_to(self, button: discord.Button, interaction: discord.Interaction):
         ...
