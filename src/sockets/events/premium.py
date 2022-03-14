a81575ca01263671e0c0ac1392ee08960a850740
deadaf <askpygod@gmail.com>
2022-03-14 22:49:50 +0530

    SockPrime cog for premium events

--- src/sockets/events/premium.py
+++ src/sockets/events/premium.py
@@ -1,10 +1,14 @@
 from __future__ import annotations
+from contextlib import suppress
 import typing as T
 
 if T.TYPE_CHECKING:
     from core import Quotient
 
 from core import Cog
+import discord
+
+from constants import random_greeting, random_thanks
 
 __all__ = ("SockPrime",)
 
@@ -30,4 +34,41 @@ class SockPrime(Cog):
 
     @Cog.listener()
     async def on_request__new_premium(self, u, data: dict):
-        user_id = int(data["user_id"])
+        if not data.get("is_verified"):
+            return await self.__transaction_failed(int(u))
+
+        await self.bot.sio.emit("new_premium__{0}".format(u), {})
+
+        user_id = int(u)
+        invoice = data["invoice_link"]
+
+        prime = "https://discord.com/oauth2/authorize?client_id=902856923311919104&scope=applications.commands%20bot&permissions=21175985838"
+
+        member = self.bot.server.get_member(user_id)
+        if member is not None:
+            await member.add_roles(discord.Object(id=self.bot.config.PREMIUM_ROLE), reason="They purchased premium.")
+
+        else:
+            member = await self.bot.getch(self.bot.get_user, self.bot.fetch_user, user_id)
+
+        _e = discord.Embed(color=self.bot.color)
+        _e.description = (
+            f"{random_greeting()} {member.mention},\n"
+            "Thanks for purchasing Quotient Premium.\n\n"
+            f"[Invite Prime Bot]({prime}) | [Support Server]({self.bot.config.SERVER_LINK}) | [Download Invoice]({invoice})"
+        )
+
+        _e.set_image(url=random_thanks())
+
+        with suppress(discord.HTTPException, AttributeError):
+            await member.send(embed=_e)
+
+    async def __transaction_failed(self, user_id: int):
+        # user = await self.bot.getch(self.bot.get_user, self.bot.fetch_user, user_id)
+        # if user:
+        #     with suppress(discord.HTTPException):
+        #         await user.send(
+        #             embed=discord.Embed()
+        #         )
+
+        ...
