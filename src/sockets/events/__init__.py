9f5f0bda1ed3f36cb42cf7572cadc04d93aa47f6
Ritik Ranjan <37435729+ritik0ranjan@users.noreply.github.com>
2022-04-17 23:04:24 +0530

    Fixed import

--- src/sockets/events/__init__.py
+++ src/sockets/events/__init__.py
@@ -2,4 +2,4 @@ from .dashgate import *  # noqa: F401, F403
 from .scrims import *  # noqa: F401, F403
 from .settings import *  # noqa: F401, F403
 from .premium import *  # noqa: F401, F403
-from .guilds   # noqa: F401, F403
+from .guilds import *  # noqa: F401, F403
