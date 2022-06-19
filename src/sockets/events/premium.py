4bdb118410f074dd5cd49b1484b5bf5fff06546d
deadaf <askpygod@gmail.com>
2022-06-19 20:11:46 +0530

    remove flantic from perks

--- src/sockets/events/premium.py
+++ src/sockets/events/premium.py
@@ -31,8 +31,7 @@ class SockPrime(Cog):
                 "Unlimited media partner channels.",
                 "Unlimited ssverification channels.",
                 "Premium role in our server + several other benefits...",
-                "Flantic Bot Premium for 1 Month (worth $3)",
-                "Real Bot Premium for 1 Month (worth $3)",
+                "Real Bot Premium for 1 Month (worth $5)",
             ],
         )
 
