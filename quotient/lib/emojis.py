388ed66d3434b0a4aaab78663baa51bb7e764248
deadaf <gahlyanrohit18@gmail.com>
2024-06-16 23:41:19 +0530

    feat: add func to conv int to emoji

--- quotient/lib/emojis.py
+++ quotient/lib/emojis.py
@@ -4,3 +4,19 @@ CROSS = "<:xmark:807913737805234176>"
 DIAMOND = "<a:diamond:899295009289949235>"  # Quotient Premium
 
 INFO = "<:info2:899020593188462693>"  # blue round i symbol
+
+
+def regional_indicator(c: str) -> str:
+    """Returns a regional indicator emoji given a character."""
+    return chr(0x1F1E6 - ord("A") + ord(c.upper()))
+
+
+def keycap_digit(c: int | str) -> str:
+    """Returns a keycap digit emoji given a character."""
+    c = int(c)
+    if 0 < c < 10:
+        return str(c) + "\U0000FE0F\U000020E3"
+    if c == 10:
+        return "\U000FE83B"
+
+    raise ValueError("Invalid keycap digit")
