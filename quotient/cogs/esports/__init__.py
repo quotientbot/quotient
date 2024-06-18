a5956277933c64b4b3ab593ec5b03267420f5aaa
deadaf <gahlyanrohit18@gmail.com>
2024-06-19 00:05:22 +0530

    feat: scrims msg command

--- quotient/cogs/esports/__init__.py
+++ quotient/cogs/esports/__init__.py
@@ -7,16 +7,30 @@ if T.TYPE_CHECKING:
 
 import discord
 from core import Context
-from discord import app_commands
 from discord.ext import commands
+from models import Scrim
 
 from .events import ScrimsEvents
 from .slash import ScrimSlashCommands
+from .views.scrims.main_panel import ScrimsMainPanel
 
 
 class Esports(commands.Cog):
     def __init__(self, bot: Quotient) -> None:
         self.bot = bot
+
+    @commands.command(name="scrims", aliases=("sm", "s"))
+    async def smanager(self, ctx: Context) -> None:
+        """
+        Manage scrims of the server.
+        """
+        if not any((ctx.author.guild_permissions.manage_guild, Scrim.is_ignorable(ctx.author))):
+            return await ctx.send(
+                embed=self.bot.error_embed("You need `scrims-mod` role or `Manage-Server` permissions to use this command.")
+            )
+
+        view = ScrimsMainPanel(ctx)
+        view.message = await ctx.send(embed=await view.initial_msg(), view=view)
 
 
 async def setup(bot: Quotient) -> None:
